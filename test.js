dbfea42da7e357200c786300da3fea3342a94a26
noxton59 <noxton59@gmail.com>
2023-09-04 13:00:16 +0500

    applied sorting

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 const routineList = JSON.parse(localStorage.getItem('routineList')) || [];
-renderRoutine();
+renderRoutine(routineList.sort(compareDate));
+let sortedListByDate = true;
+let sortedByDateBack = false;
+let sortedListByName = false;
+let sortedByNameBack = false;
 
 
 function enter() {
@@ -21,28 +25,40 @@ function enter() {
     routine.value = '';
     sets.value = "";
     reps.value = "";
-    renderRoutine();
+    if (sortedListByDate === true && sortedByDateBack === false) {
+      renderRoutine(routineList.sort(compareDate));
+    }
+    else if (sortedListByDate === true && sortedByDateBack === true) {
+      renderRoutine(routineList.sort(compareDateBack));
+    }
+    else if (sortedListByName === true && sortedByNameBack === false) {
+      renderRoutine(routineList.sort(compareNameBack));
+    }
+    else if (sortedListByName === true && sortedByNameBack === true) {
+      renderRoutine(routineList.sort(compareName));
+    }
+    
     saveToStor();
     document.querySelector('.alert-message').classList.remove("is-alert");
   }
-  
 }
 
-function renderRoutine() {
+function renderRoutine(sortedRoutine) {
   let RoutineHTML = '';
-  routineList.forEach(function(routineObject) {
+  sortedRoutine.forEach(function(routineObject) {
     const {name, reps, sets, date, id} = routineObject;
     let row = `
     <div class="js-container-${id} container">
       <div class="name-div">${name}</div>
-      <div class="rep-div">repetitions: ${reps}</div>
-      <div class="set-div">sets: ${sets}</div>
+      <div class="rep-div js-rep-div-${id}">repetitions: ${reps}</div>
+      <div class="set-div js-set-div-${id}">sets: ${sets}</div>
       <div class="date-div">date: ${date}</div>
       <button class="delete-button js-delete-button" data-delete-id=${id}>Delete</button>
       <button class="update-button js-update-button" data-update-id=${id}>Update</button>
-      <input class="rep-input js-rep-input js-input-rep-${id}" type="number" placeholder="Reps" data-input-rep-id=${id}>
-      <input class="set-input js-set-input js-input-set-${id}" type="number" placeholder="Sets" data-input-set-id=${id}>
+      <input class="rep-input js-rep-input js-input-rep-${id}" type="number/text" min="0" placeholder="Reps" data-input-rep-id=${id}>
+      <input class="set-input js-set-input js-input-set-${id}" type="number" min="0" placeholder="Sets" data-input-set-id=${id}>
       <button class="save-button js-save-button" data-save-id=${id}>Save</button>
+      <div class="alert-message-reps-sets js-alert-message-reps-sets-${id}">Input reps and sets</div>
     </div>`
     RoutineHTML += row;
   })
@@ -57,7 +73,18 @@ function renderRoutine() {
           if (item.id === exerId)
           routineList.splice(routineList.indexOf(item), 1);
           container.remove();
-          renderRoutine();
+          if (sortedListByDate === true && sortedByDateBack === false) {
+            renderRoutine(routineList.sort(compareDate));
+          }
+          else if (sortedListByDate === true && sortedByDateBack === true) {
+            renderRoutine(routineList.sort(compareDateBack));
+          }
+          else if (sortedListByName === true && sortedByNameBack === false) {
+            renderRoutine(routineList.sort(compareNameBack));
+          }
+          else if (sortedListByName === true && sortedByNameBack === true) {
+            renderRoutine(routineList.sort(compareName));
+          }
           saveToStor();
         }) 
       }
@@ -72,19 +99,27 @@ function renderRoutine() {
 
   document.querySelectorAll(".js-save-button").forEach((button)=>{
     button.addEventListener('click', ()=>{
-      document.querySelector(`.js-container-${button.dataset.saveId}`).classList.remove("is-updating");
-      let newSets = Number(document.querySelector(`.js-input-set-${button.dataset.saveId}`).value);
-      let newReps = Number(document.querySelector(`.js-input-rep-${button.dataset.saveId}`).value);
       const saveButtonId = button.dataset.saveId;
-      routineList.forEach((routine)=>{
-        if (routine.id == saveButtonId) {
-          routine.sets = newSets;
-          routine.reps = newReps;
-          renderRoutine();
-          saveToStor();
-        }
-        
-      })
+      let newSets = Number(document.querySelector(`.js-input-set-${button.dataset.saveId}`).value);
+      let newReps = document.querySelector(`.js-input-rep-${button.dataset.saveId}`).value;
+      if (newSets == 0 || newReps == 0) {
+        document.querySelector(`.js-alert-message-reps-sets-${saveButtonId}`).classList.add('empty-fields');
+      }
+      else {
+        document.querySelector(`.js-container-${button.dataset.saveId}`).classList.remove("is-updating");
+        document.querySelector(`.js-alert-message-reps-sets-${saveButtonId}`).classList.remove('empty-fields');
+        routineList.forEach((routine)=>{
+          if (routine.id == saveButtonId) {
+              routine.sets = newSets;
+              routine.reps = newReps;
+              document.querySelector(`.js-rep-div-${saveButtonId}`).innerHTML = `repetitions: ${routine.reps}`;
+              document.querySelector(`.js-set-div-${saveButtonId}`).innerHTML = `sets: ${routine.sets}`;
+              saveToStor();
+              document.querySelector(`.js-input-rep-${button.dataset.saveId}`).value = '';
+              document.querySelector(`.js-input-set-${button.dataset.saveId}`).value = '';
+          }
+        })
+      }
     })
   })
 }
@@ -116,9 +151,10 @@ function displayExercise(exercises) {
       <div class="date-div">date: ${exercise.date}</div>
       <button class="delete-button js-delete-button" data-delete-id=${exercise.id}>Delete</button>
       <button class="update-button js-update-button" data-update-id=${exercise.id}>Update</button>
-      <input class="rep-input js-rep-input js-input-rep-${exercise.id}" type="number" placeholder="Reps" data-input-rep-id=${exercise.id}>
+      <input class="rep-input js-rep-input js-input-rep-${exercise.id}" type="number/text" placeholder="Reps" data-input-rep-id=${exercise.id}>
       <input class="set-input js-set-input js-input-set-${exercise.id}" type="number" placeholder="Sets" data-input-set-id=${exercise.id}>
       <button class="save-button js-save-button" data-save-id=${exercise.id}>Save</button>
+      <div class="alert-message-reps-sets js-alert-message-reps-sets-${exercise.id}">Input reps and sets</div>
     </div>`})
     .join('');
   document.querySelector('.js-routine-list').innerHTML = row;
@@ -145,24 +181,27 @@ function displayExercise(exercises) {
       document.querySelector(`.js-container-${button.dataset.updateId}`).classList.add("is-updating");
     })
   })
-
   document.querySelectorAll(".js-save-button").forEach((button)=>{
     button.addEventListener('click', ()=>{
-      document.querySelector(`.js-container-${button.dataset.saveId}`).classList.remove("is-updating");
+      const saveButtonId = button.dataset.saveId;
       let newSets = Number(document.querySelector(`.js-input-set-${button.dataset.saveId}`).value);
       let newReps = Number(document.querySelector(`.js-input-rep-${button.dataset.saveId}`).value);
-      const saveButtonId = button.dataset.saveId;
-      routineList.forEach((routine)=>{
-        if (routine.id == saveButtonId) {
-          routine.sets = newSets;
-          routine.reps = newReps;
-          const searchString = filter.value;
-          const filtered = routineList.filter((exercise) => {return (exercise.name.includes(searchString));});
-          displayExercise(filtered);
-          saveToStor();
-        }
-        
-      })
+      if (newSets == 0 || newReps == 0) {
+        document.querySelector(`.js-alert-message-reps-sets-${saveButtonId}`).classList.add('empty-fields');
+      }
+      else {
+        routineList.forEach((routine)=>{
+          if (routine.id == saveButtonId) {
+              document.querySelector(`.js-container-${button.dataset.saveId}`).classList.remove("is-updating");
+              routine.sets = newSets;
+              routine.reps = newReps;
+              const searchString = filter.value;
+              const filtered = routineList.filter((exercise) => {return (exercise.name.includes(searchString));});
+              displayExercise(filtered);
+              saveToStor();
+          }
+        })
+      }
     })
   })
 }
@@ -183,3 +222,70 @@ document.querySelector('.js-date').addEventListener('focus', ()=>{
 })
 
 
+function compareDate (a,b) {
+  if (a.date < b.date) {
+    return 1;
+  }
+  if (a.date > b.date) {
+    return -1;
+  }
+  return 0;
+}
+
+function compareDateBack (a,b) {
+  if (a.date < b.date) {
+    return -1;
+  }
+  if (a.date > b.date) {
+    return 1;
+  }
+  return 0;
+}
+
+function compareName (a,b) {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+}
+
+function compareNameBack (a,b) {
+  if (a.name < b.name) {
+    return 1;
+  }
+  if (a.name > b.name) {
+    return -1;
+  }
+  return 0;
+}
+
+document.querySelector('.js-sort-by-name-btn').addEventListener('click', ()=>{
+  sortedListByName = true;
+  sortedListByDate = false;
+  sortedByDateBack = false;
+  if (sortedListByName === true && sortedByNameBack === false) {
+    renderRoutine(routineList.sort(compareName));
+    sortedByNameBack = true;
+  }
+  else if (sortedListByName === true && sortedByNameBack === true) {
+    renderRoutine(routineList.sort(compareNameBack));
+    sortedByNameBack = false;
+  }
+})
+
+document.querySelector('.js-sort-by-date-btn').addEventListener('click', ()=>{
+  sortedListByDate = true;
+  sortedListByName = false;
+  sortedByNameBack = false;
+  if (sortedListByDate === true && sortedByDateBack === false) {
+    renderRoutine(routineList.sort(compareDateBack));
+    sortedByDateBack = true;
+  }
+  else if (sortedListByDate === true && sortedByDateBack === true) {
+    renderRoutine(routineList.sort(compareDate));
+    sortedByDateBack = false;
+  }
+});
